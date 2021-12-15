12539a37cd09d03747e94dc318271c9537e3610e
MulualemBerhanu <89715104+MulualemBerhanu@users.noreply.github.com>
2021-12-15 13:16:39 -0600

    registration

--- src/main/java/miu/edu/com/courseregistrationsystem/service/implementation/StudentServiceImpl.java
+++ src/main/java/miu/edu/com/courseregistrationsystem/service/implementation/StudentServiceImpl.java
@@ -1,10 +1,10 @@
 package miu.edu.com.courseregistrationsystem.service.implementation;
 
-<<<<<<< HEAD
+
 import miu.edu.com.courseregistrationsystem.domain.RegistrationEvent;
-=======
+
 import miu.edu.com.courseregistrationsystem.domain.RegistrationRequest;
->>>>>>> 47d0020c2ba4fcad2ee66db4fd93ff99ea3e39e8
+
 import miu.edu.com.courseregistrationsystem.domain.Student;
 import miu.edu.com.courseregistrationsystem.repository.StudentRepository;
 import miu.edu.com.courseregistrationsystem.service.StudentService;
