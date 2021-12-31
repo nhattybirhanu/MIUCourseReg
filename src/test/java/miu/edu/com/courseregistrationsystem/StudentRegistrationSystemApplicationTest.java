0fb83784cafad288f85d17e714babcfb239bd5b6
nhattybirhanu <nhattybirhanu@gmail.com>
2021-12-31 14:53:35 -0600

    force fully fail

--- src/test/java/miu/edu/com/courseregistrationsystem/StudentRegistrationSystemApplicationTest.java
+++ src/test/java/miu/edu/com/courseregistrationsystem/StudentRegistrationSystemApplicationTest.java
@@ -1,14 +1,17 @@
 package miu.edu.com.courseregistrationsystem;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
+@Slf4j
 class StudentRegistrationSystemApplicationTest {
 @Test
 
-    public void  test_1(){
+    public  test_1(){
+    log.info("Testing phase");
     assertEquals(true,true);
 }
 }
