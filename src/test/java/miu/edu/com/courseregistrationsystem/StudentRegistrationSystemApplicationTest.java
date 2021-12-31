1468d9d2be52c78de6749e55092160e8bd74a11c
nhattybirhanu <nhattybirhanu@gmail.com>
2021-12-31 10:37:54 -0600

    test

---
+++ src/test/java/miu/edu/com/courseregistrationsystem/StudentRegistrationSystemApplicationTest.java
@@ -0,0 +1,14 @@
+package miu.edu.com.courseregistrationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+class StudentRegistrationSystemApplicationTest {
+@Test
+
+    public void  test_1(){
+    assertEquals(true,true);
+}
+}
